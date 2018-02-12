ffc00f88d8a2b596909707c0d11f0efbef29e5f6
loenko <loenko.js@gmail.com>
2018-02-13 00:44:00 +0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My Days Of Code',
+    author: 'Fyodor Loenko'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ],
+};
---
+++ src/pages/02-12-2018-getting-started/index.md
@@ -0,0 +1,7 @@
+---
+path: "/getting-started"
+date: "2018-02-12T18:00:00.000Z"
+title: "I'm Getting Started"
+---
+
+This is my first blog post! I'm gonna log my failures and achievements on the neverending learning way using one of the tools learnt recently - Gatsby - React-based static site generator. It's a part of my [#100DaysOfCode](https://twitter.com/search?q=%23100DaysOfCode) program which I [log daily](https://github.com/loenko/100-days-of-code/blob/master/log.md) and want to summarize weekly from now on.
