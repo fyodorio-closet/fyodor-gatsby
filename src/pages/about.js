633b4517d72aa0f789dc870745429b30e63dd656
loenko <loenko.js@gmail.com>
2018-03-08 00:46:09 +0500

    Adde about info

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,27 @@ const AboutPage = () => (
   <div>
     <Helmet title={`About - My Days Of Code`} />
     <h1>Who Am I?</h1>
-    <p>My name is Fyodor. I'm a JavaScript Developer.</p>
+    <p>My name is Fyodor. I'm a Software Developer. I use HTML, CSS and JavaScript to create web applications with a focus on simplicity, both in design and in implementation.</p>
+    <p>My tools of choice are</p>
+    <ul>
+      <li>Semantic HTML and Web Components</li>
+      <li>SASS for CSS preprocessing</li>
+      <li>Latest ECMAScript</li>
+      <li>Webpack for module transpiling and bundling the sources</li>
+      <li>Karma+Mocha+Chai+Sinon for Unit Testing</li>
+      <li>ESLint for code checks</li>
+      <li>Yarn for running all the scripts and automation</li>
+      <li>Git for Version Control</li>
+      <li>AngularJS / Angular for severe mood</li>
+      <li>React for sassy mood</li>
+      <li>Bootstrap for fast app prototyping</li>
+      <li>Gatsby.js for building solid and simple web pages (e.g. this one)</li>
+      <li>Codepen for experiments and Code Art</li>
+      <li>Functional Programming as a preffered coding paradigm</li>
+      <li>VSCode as an IDE</li>
+      <li>A pencil and a paper for design mockups</li>
+    </ul>
+    <p>I'm a passionate learner and try to get new knowledge with any opportunity. That's why I <a target="_blank" href="https://github.com/loenko/100-days-of-code/blob/master/log.md">participate in #100DaysOfCode challenge</a> and use this blog to record some related thoughts.</p>
   </div>
 )
 
