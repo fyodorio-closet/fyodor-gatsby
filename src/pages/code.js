85b4bc55c9973ed932bb11e1f72a5522dabeba6f
loenko <loenko.js@gmail.com>
2018-03-07 20:40:10 +0500

    Added social accounts and font-awesome packages for them

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
 
 const CodePage = () => (
   <div>
     <Helmet title={`Code - My Days Of Code`} />
     <h1>My Code Portfolio</h1>
     <p>You can find the examples of my work here.</p>
-    <Link to="/">Go back to the homepage</Link>
   </div>
 )
 
--- src/pages/connect.js
+++ src/pages/connect.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
+/* react-fontawesome documentation is at 
+https://github.com/FortAwesome/react-fontawesome */
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/fontawesome-free-solid'; 
+import { faTelegramPlane } from '@fortawesome/fontawesome-free-brands';
+import { faTwitter } from '@fortawesome/fontawesome-free-brands';
+import { faCodepen } from '@fortawesome/fontawesome-free-brands';
+import { faGithub } from '@fortawesome/fontawesome-free-brands';
+
+import './connect.scss';
+
+const iconSize = '2x';
 
 const ConnectPage = () => (
   <div>
     <Helmet title={`Connect - My Days Of Code`} />
     <h1>Let's Get In Touch</h1>
-    <p>You can find my contacts and social accounts here.</p>
-    <Link to="/">Go back to the homepage</Link>
+    <ul className="accounts">
+      <li className="account">
+        <a target="_blank" href="mailto:fyodor.loenko@mailhero.io">
+          <FontAwesomeIcon icon={faEnvelope} size={iconSize} />
+        </a>
+        <div className="account-name">fyodor.loenko@mailhero.io</div>
+      </li>
+      <li className="account">
+        <a target="_blank" href="https://t.me/loenko">
+          <FontAwesomeIcon icon={faTelegramPlane} size={iconSize} />
+        </a>
+        <div className="account-name">@loenko</div>
+      </li>
+      <li className="account">
+        <a target="_blank" href="https://twitter.com/fyodorloenko">
+          <FontAwesomeIcon icon={faTwitter} size={iconSize} />
+        </a>
+        <div className="account-name">@fyodorloenko</div>
+      </li>
+      <li className="account">
+        <a target="_blank" href="https://github.com/loenko">
+          <FontAwesomeIcon icon={faGithub} size={iconSize} />
+        </a>
+        <div className="account-name">@loenko</div>
+      </li>
+      <li className="account">
+        <a target="_blank" href="https://codepen.io/loenko/">
+          <FontAwesomeIcon icon={faCodepen} size={iconSize} />
+        </a>
+        <div className="account-name">@loenko</div>
+      </li>
+    </ul>
+    <br />
   </div>
 )
 
