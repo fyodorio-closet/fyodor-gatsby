5811f88f7ab80f94b5899f951bd6b7eec8bbd66c
loenko <loenko.js@gmail.com>
2018-11-15 18:02:12 +0500

    Fixed build error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import Header from '../components/Header';
+import Header from '../components/header';
 import './layout.scss';
 
 const TemplateWrapper = ({ children }) => (
