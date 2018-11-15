c22bbc3f3f4bfdb14c1d2f7a67c6abcc368a1fce
loenko <loenko.js@gmail.com>
2018-11-15 22:17:12 +0500

    Format icons on connect page

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -16,31 +16,31 @@ const ConnectPage = () => (
       <h1>Let's Get In Touch</h1>
       <ul className="accounts">
         <li className="account">
-          <a target="_blank" rel="noopener noreferrer" href="mailto:fyodor.loenko@mailhero.io">
+          <a className="account-link" target="_blank" rel="noopener noreferrer" href="mailto:fyodor.loenko@mailhero.io">
             <FontAwesomeIcon icon={faEnvelope} size={iconSize} />
           </a>
           <div className="account-name">fyodor.loenko@mailhero.io</div>
         </li>
         <li className="account">
-          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/fyodorio">
+          <a className="account-link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/fyodorio">
             <FontAwesomeIcon icon={faTwitter} size={iconSize} />
           </a>
           <div className="account-name">@fyodorio</div>
         </li>
         <li className="account">
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/loenko">
+          <a className="account-link" target="_blank" rel="noopener noreferrer" href="https://github.com/loenko">
             <FontAwesomeIcon icon={faGithub} size={iconSize} />
           </a>
           <div className="account-name">@loenko</div>
         </li>
         <li className="account">
-          <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/loenko/">
+          <a className="account-link" target="_blank" rel="noopener noreferrer" href="https://codepen.io/loenko/">
             <FontAwesomeIcon icon={faCodepen} size={iconSize} />
           </a>
           <div className="account-name">@loenko</div>
         </li>
         <li className="account">
-          <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/6778546/fyodor">
+          <a className="account-link" target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/6778546/fyodor">
             <FontAwesomeIcon icon={faStackOverflow} size={iconSize} />
           </a>
           <div className="account-name">@6778546</div>
